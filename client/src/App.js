b0eb4efad4f275d2d1578589c84b6a918c78ffe5
DESKTOP-E6U9IT1\t2sun <t2suna@gmail.com>
2023-03-18 00:14:15 +0900

    add client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import EssayForm from "./components/EssayForm";
+
+
+
+export default function Mysite() {
+  return (
+    <>
+    <EssayForm />
+    </>
+  );
+}
---
+++ client/src/components/EssayForm.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import axios from 'axios';
+
+export default class EssayForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 'めもだよっ'
+      };
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      let params = new URLSearchParams();
+      params.append('apikey', this.state.value);
+      axios.post("https://localhost:8080/post", params)
+        .then(function (response) {
+          // 送信成功時の処理
+          console.log(response);
+          alert('メモが保存されました: ' + this.state.value);
+        })
+        .catch(function (error) {
+          // 送信失敗時の処理
+          console.log(error);
+          alert(error);
+        });
+        
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <textarea value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+  
